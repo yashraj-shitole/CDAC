bd00b1ced5355fa6ebda459e74bd007dcc66dba6
YASHRAJ SHITOLE <yashrajs927@gmail.com>
2024-12-27 15:02:48 +0530

    DotNET Day02

---
+++ DotNET/Day02/KDAC_AUG_24/MathsLib/Maths.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsLib
+{
+    #region Maths Class Code
+    public class Maths
+    {
+        #region Add Function Code
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        #endregion
+
+        #region Sub Function Code
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        #endregion
+    }
+    #endregion
+}
