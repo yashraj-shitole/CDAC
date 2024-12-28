ccdd292f48d3dc070b482e2cdf136e608d0ff1c1
YASHRAJ SHITOLE <yashrajs927@gmail.com>
2024-12-28 15:20:58 +0530

    DotNET

---
+++ DotNET/Day03/03DemoOOP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03DemoOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IY obj1 = new Maths();  
+           
+        }
+    }
+
+    public interface IX
+    {
+        int Add(int x, int y);
+        int Sub(int x, int y);
+    }
+
+
+    public interface IY
+    {
+        int Mult(int x, int y);
+        int Div(int x, int y);
+    }
+
+    public class Maths: IX, IY
+    {
+        public int Add(int x, int y) { 
+            return x + y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+
+}
